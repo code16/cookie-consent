da30c6833f89361b52af9192971e5fda513aeed3
antoine <antoine.guingand@wanadoo.fr>
2020-02-18 12:00:28 +0100

    Remove generated license file

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,7 @@ mix.js('resources/assets/js/index.js', 'cookie-consent.js')
     .setPublicPath('resources/assets/dist')
     .options({
         processCssUrls: false,
+        terser: {
+            extractComments: false,
+        }
     });
